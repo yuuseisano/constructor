dc8be1d9d17b2a6290007e8215899b021b7819da
4D-19\koyo <a>
2026-05-26 15:01:10 +0900

    例５

--- konnsutorakuta/konnsutorakuta.cpp
+++ konnsutorakuta/konnsutorakuta.cpp
@@ -17,54 +17,34 @@ class Player
 public:
 
 	std::string Name;
-	int HP;
-	int MP;
-	int ATK;
-	int DEF;
-	int SPD;
+	PlayerSettings Settings;
 
 
-
-	Player(std::string name,const PlayerSettings& settings)
-		:Player(name, settings.HP, settings.MP, settings.ATK, settings.DEF, settings.SPD)
-	{
-	}
-
-	Player() 
-		:Player("Default")
-	{
-	}
-
-	Player(std::string name, int hp = 100,int mp = 0,int atk = 10,int def = 5,int spd = 5)
-		:Name(name), HP(hp), MP(mp), ATK(atk), DEF(def), SPD(spd)
-	{
-	}
 
 	Player(const Player& other) 
-		:Name(other.Name), HP(other.HP), MP(other.MP), ATK(other.ATK), DEF(other.DEF), SPD(other.SPD)
+		:Name(other.Name),Settings(other.Settings)
 	{
-
 	}
+	Player(std::string name, const PlayerSettings& settings)
+		:Name(name), Settings(settings)
+	{
+	}
+
 };
 void Show(const Player& player) {
 	std::cout << "name: " << player.Name
-		<< " HP:" << player.HP
-		<< " MP:" << player.MP
-		<< " ATK:" << player.ATK
-		<< " DEF:" << player.DEF
-		<< " SPD:" << player.SPD
+		<< " HP:" << player.Settings.HP
+		<< " MP:" << player.Settings.MP
+		<< " ATK:" << player.Settings.ATK
+		<< " DEF:" << player.Settings.DEF
+		<< " SPD:" << player.Settings.SPD
 		<< std::endl;
 }
 
 int main()
 {
 
-	Player player1;
-	Player player2("勇者", 200,0,10,5,1);
-	Player player3("勇者",hero);
+	Player player("勇者", hero);
 
-	Show(player1);
-	Show(player2);
-	Show(player3);
+	Show(player);
 }
-
