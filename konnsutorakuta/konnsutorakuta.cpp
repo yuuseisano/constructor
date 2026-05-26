0763188cdd2a39868814f3302dc6d5e7b3c60f6f
4D-19\koyo <a>
2026-05-26 14:07:55 +0900

    コピーコンストラクタ

--- konnsutorakuta/konnsutorakuta.cpp
+++ konnsutorakuta/konnsutorakuta.cpp
@@ -24,6 +24,11 @@ public:
 	{
 	}
 
+	Player(const Player& other) 
+		:Name(other.Name), HP(other.HP), MP(other.MP), ATK(other.ATK), DEF(other.DEF), SPD(other.SPD)
+	{
+
+	}
 };
 void Show(const Player& player) {
 	std::cout << "name: " << player.Name
@@ -38,7 +43,7 @@ void Show(const Player& player) {
 int main()
 {
 	Player player1;
-	Player player2("勇者", 200);
+	Player player2("勇者", 200,0,10,5,1);
 
 	Show(player1);
 	Show(player2);
