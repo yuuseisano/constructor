9b5f9a1473a963543648e12339137ea09aeb2c91
4D-19\koyo <a>
2026-05-26 13:58:42 +0900

    例３

--- konnsutorakuta/konnsutorakuta.cpp
+++ konnsutorakuta/konnsutorakuta.cpp
@@ -7,23 +7,40 @@ public:
 
 	std::string Name;
 	int HP;
+	int MP;
+	int ATK;
+	int DEF;
+	int SPD;
+
+
 
 	Player() 
 		:Player("Default",100)
 	{
 	}
 
-	Player(std::string name, int hp)
-		:Name(name), HP(hp)
+	Player(std::string name, int hp = 100,int mp = 0,int atk = 10,int def = 5,int spd = 5)
+		:Name(name), HP(hp), MP(mp), ATK(atk), DEF(def), SPD(spd)
 	{
 	}
+
 };
+void Show(const Player& player) {
+	std::cout << "name: " << player.Name
+		<< " HP:" << player.HP
+		<< " MP:" << player.MP
+		<< " ATK:" << player.ATK
+		<< " DEF:" << player.DEF
+		<< " SPD:" << player.SPD
+		<< std::endl;
+}
+
 int main()
 {
 	Player player1;
 	Player player2("勇者", 200);
 
-	std::cout <<"name: " << player1.Name << " HP:" << player1.HP << std::endl;
-	std::cout <<"name: " << player2.Name << " HP:"<<player2.HP << std::endl;
+	Show(player1);
+	Show(player2);
 }
 
